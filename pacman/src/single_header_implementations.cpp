98c2e5375083504bbaab4f5d98e4cecf262d906f
Carl Findahl <sportleroro@gmail.com>
2019-02-04 19:25:20 +0100

    Add GFX header from the official IMT2531 course repo

---
+++ pacman/src/single_header_implementations.cpp
@@ -0,0 +1,7 @@
+/*!
+ * \file This file includes all single-header libraries that use the #define XXX_IMPLEMENTATION idiom to
+ * keep the amount of corresponding cpp files manageable.
+ */
+
+#define GFX_IMPLEMENTATION
+#include "gfx.h"
