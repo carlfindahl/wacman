b64d17fca78aad1379112f5fa459f0a563eeabd3
Carl Findahl <sportleroro@gmail.com>
2019-02-08 08:46:15 +0100

    Add UBO to gamestate
    Add relevant UBO to game state so it can decide how it wants things to be projected and rendered. Also adds a testing draw call to draw.

--- pacman/src/states/game_state.h
+++ pacman/src/states/game_state.h
@@ -1,14 +1,32 @@
 #pragma once
 
+#include <rendering/vertex_array_object.h>
+#include <rendering/uniform_buffer_object.h>
 #include <states/state.h>
 
-namespace pac {
+#include <glm/mat4x4.hpp>
+
+namespace pac
+{
+namespace detail
+{
+struct MatrixData
+{
+    glm::mat4 proj_matrix = glm::mat4(1.f);
+    glm::mat4 view_matrix = glm::mat4(1.f);
+};
+}  // namespace detail
+
 class GameState : public State
 {
 private:
+    /* Matrix UBO */
+    UniformBuffer<detail::MatrixData> m_ubo = {};
 
 public:
     GameState();
+
+    ~GameState() noexcept override;
 
     void on_enter() override;
 
@@ -18,4 +36,4 @@ public:
 
     bool draw() override;
 };
-} // namespace pac
+}  // namespace pac
