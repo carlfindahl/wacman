a4c8db42e04ebe8d9e39ee6b3885bf3bb55c83d8
Carl Findahl <sportleroro@gmail.com>
2019-02-19 09:33:31 +0100

    Add high score table and file to read results from
    You can now read results and show the high scores in a separate Score table.

--- pacman/src/states/high_score_state.cpp
+++ pacman/src/states/high_score_state.cpp
@@ -3,6 +3,9 @@
 #include "config.h"
 #include "input.h"
 
+#include <fstream>
+
+#include <imgui/imgui.h>
 #include <GLFW/glfw3.h>
 
 namespace pac
@@ -17,16 +20,76 @@ void HighScoreState::on_enter()
     input::InputState hs_input_state(true);
     hs_input_state.set_binding(GLFW_KEY_ESCAPE, [this] { m_owner->pop(); });
     input::get_input().push(std::move(hs_input_state));
+
+    load_entries_from_file();
 }
 
 void HighScoreState::on_exit() { input::get_input().pop(); }
 
-bool HighScoreState::update(float dt) { return false; }
+bool HighScoreState::update(float dt)
+{
+    /* Position high score table properly */
+    ImGui::SetNextWindowPos({SCREEN_W / 2.f, SCREEN_H / 2.f + 60.f}, 0, {.5f, .5f});
+    ImGui::SetNextWindowSize({400.f, 500.f});
+    ImGui::Begin("High Score", nullptr, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+
+    /* Set up a 3 column high score table */
+    ImGui::Columns(3, "ScoreColumns", false);
+    ImGui::SetColumnWidth(0, 25.f);
+    ImGui::SetColumnWidth(1, 275.f);
+
+    /* Print score for each element */
+    int i = 1;
+    for (const auto& e : m_entries)
+    {
+        ImGui::Text("%d", i++);
+        ImGui::NextColumn();
+        ImGui::Text("%s", e.name.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%d", e.score);
+        ImGui::NextColumn();
+        ImGui::Separator();
+    }
+
+    /* Reset columns and add a back buton */
+    ImGui::Columns();
+    if (ImGui::Button("Back"))
+    {
+        m_owner->pop();
+    }
+
+    ImGui::End();
+    return false;
+}
 
 bool HighScoreState::draw()
 {
     get_renderer().draw({{SCREEN_W / 2.f, SCREEN_H / 2.f}, glm::vec2(SCREEN_W, SCREEN_H), {1.f, 1.f, 1.f}, m_splash_texture});
     return false;
+}
+
+void HighScoreState::load_entries_from_file()
+{
+    /* Open file to read */
+    std::ifstream file(cgl::native_absolute_path("res/highscores.txt"));
+
+    /* Store each new entry temporarily in e, then based on the line count, read data into line and parse relevant information */
+    Entry e;
+    int line_count = 0;
+    std::string line{};
+    while (std::getline(file, line))
+    {
+        switch (line_count % 2)
+        {
+        case 0: e.name = line; break;
+        case 1:
+            e.score = std::stoi(line);
+            m_entries.push_back(std::move(e));
+            break;
+        }
+
+        ++line_count;
+    }
 }
 
 }  // namespace pac
--- pacman/src/states/high_score_state.h
+++ pacman/src/states/high_score_state.h
@@ -3,13 +3,28 @@
 #include "state.h"
 #include "rendering/renderer.h"
 
+#include <vector>
+#include <string>
+
 namespace pac
 {
 class HighScoreState : public State
 {
 private:
+    /*!
+     * \brief The Entry struct represents a single entry in the high score table
+     */
+    struct Entry
+    {
+        std::string name = {};
+        int score = 0;
+    };
+
     /* Splash screen texture */
-    TextureID m_splash_texture{};
+    TextureID m_splash_texture = {};
+
+    /* All entries */
+    std::vector<Entry> m_entries = {};
 
 public:
     HighScoreState() = default;
@@ -23,6 +38,12 @@ public:
     bool update(float dt) override;
 
     bool draw() override;
+
+private:
+    /*!
+     * \brief load_entries_from_file loads high score entries from highscores.txt in order to display them in the menu
+     */
+    void load_entries_from_file();
 };
 
 }  // namespace pac
