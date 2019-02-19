3c72ed97821c3a67cc4fb671b0506fa623ff9d5c
Carl Findahl <sportleroro@gmail.com>
2019-02-19 10:34:53 +0100

    Switched pathfinding A*
    Measured performance and could see a 2x in some scenarios, and at minimum 20%, so decided to switch to A*.

--- pacman/src/pathfinding.cpp
+++ pacman/src/pathfinding.cpp
@@ -1,1 +1,115 @@
 #include "pathfinding.h"
+#include "level.h"
+#include "common.h"
+
+#include <queue>
+#include <vector>
+#include <type_traits>
+#include <unordered_map>
+
+#include <gfx.h>
+
+pac::Path::Path(const pac::Level& graph, glm::ivec2 origin, glm::ivec2 target) : m_creation_time(std::chrono::steady_clock::now())
+{
+    pathfind_astar(graph, origin, target);
+}
+
+glm::ivec2 pac::Path::get()
+{
+    if (!m_directions.empty())
+    {
+        auto val = m_directions.top();
+        m_directions.pop();
+        return val;
+    }
+
+    return {};
+}
+
+bool pac::Path::empty() const { return m_directions.empty(); }
+
+bool pac::Path::outdated() const { return (std::chrono::steady_clock::now() - m_creation_time) > std::chrono::milliseconds(90); }
+
+void pac::Path::pathfind_bfs(const pac::Level& graph, glm::ivec2 origin, glm::ivec2 target)
+{
+    /* Get neighbours and try all paths until we find target, or if we don't the Path will be empty */
+    std::queue<glm::ivec2> next_node = {};
+    next_node.push(origin);
+
+    std::unordered_map<glm::ivec2, glm::ivec2, detail::custom_ivec2_hash> traceback = {};
+    traceback[origin] = origin;
+
+    while (!next_node.empty())
+    {
+        auto current = next_node.front();
+        next_node.pop();
+
+        if (current == target)
+        {
+            break;
+        }
+
+        for (auto next : graph.get_neighbours(current))
+        {
+            if (traceback.find(next) == traceback.end())
+            {
+                next_node.push(next);
+                traceback[next] = current;
+            }
+        }
+    }
+
+    /* Do traceback into stack */
+    for (auto it = target; it != origin; it = traceback.at(it))
+    {
+        m_directions.push(it - traceback.at(it));
+    }
+}
+
+void pac::Path::pathfind_astar(const pac::Level& graph, glm::ivec2 origin, glm::ivec2 target)
+{
+    /* Get neighbours and try all paths until we find target, or if we don't the Path will be empty */
+    using priority_pair = std::pair<int, glm::ivec2>;
+    auto priority_func = [](const priority_pair& a, const priority_pair& b) { return a.first > b.first; };
+
+    /* Use a priority queue with heuristic */
+    std::priority_queue<priority_pair, std::vector<priority_pair>, decltype(priority_func)> next_node(priority_func);
+    next_node.push(priority_pair(0.0, origin));
+
+    /* Keep a traceback map and a cost map */
+    std::unordered_map<glm::ivec2, glm::ivec2, detail::custom_ivec2_hash> traceback = {};
+    std::unordered_map<glm::ivec2, int, detail::custom_ivec2_hash> cost_so_far = {};
+    traceback[origin] = origin;
+    cost_so_far[origin] = 0;
+
+    while (!next_node.empty())
+    {
+        auto current = next_node.top().second;
+        next_node.pop();
+
+        if (current == target)
+        {
+            break;
+        }
+
+        /* Apply heuristic while processing the map */
+        for (auto next : graph.get_neighbours(current))
+        {
+            int new_cost = cost_so_far[current] + 1;
+            if (cost_so_far.find(next) == cost_so_far.end() || new_cost < cost_so_far[next])
+            {
+                cost_so_far[next] = new_cost;
+                traceback[next] = current;
+                next_node.push(priority_pair(new_cost + heuristic(next, target), next));
+            }
+        }
+    }
+
+    /* Do traceback into stack */
+    for (auto it = target; it != origin; it = traceback.at(it))
+    {
+        m_directions.push(it - traceback.at(it));
+    }
+}
+
+int pac::Path::heuristic(glm::ivec2 from, glm::ivec2 to) { return std::abs(from.x - to.x) + std::abs(from.y - to.y); }
--- pacman/src/pathfinding.h
+++ pacman/src/pathfinding.h
@@ -1,20 +1,14 @@
 #pragma once
 
-#include "level.h"
-#include "common.h"
-
 #include <stack>
-#include <queue>
-#include <vector>
 #include <chrono>
-#include <type_traits>
-#include <unordered_map>
 
-#include <gfx.h>
 #include <glm/vec2.hpp>
 
 namespace pac
 {
+class Level;
+
 /*!
  * \class Path
  * \brief Path represents a stack of directions from an origin to a target.
@@ -34,74 +28,49 @@ public:
     /*!
      * \brief Construct  a Path from a type of Graph
      */
-    Path(const Level& graph, glm::ivec2 origin, glm::ivec2 target) : m_creation_time(std::chrono::steady_clock::now())
-    {
-        pathfind_bfs(graph, origin, target);
-    }
+    Path(const Level& graph, glm::ivec2 origin, glm::ivec2 target);
 
     /*!
      * \brief get the next direction from the path
      * \return the next direction to move to get to your target
      */
-    glm::ivec2 get()
-    {
-        if (!m_directions.empty())
-        {
-            auto val = m_directions.top();
-            m_directions.pop();
-            return val;
-        }
-
-        return {};
-    }
+    glm::ivec2 get();
 
     /*!
      * \brief empty returns empty if the Path does not exist
      * \return true if the path is empty
      */
-    bool empty() const { return m_directions.empty(); }
+    bool empty() const;
 
     /*!
      * \brief outdated checks if the path is outdated
      * \return true if the path is considered outdated.
      */
-    bool outdated() const { return (std::chrono::steady_clock::now() - m_creation_time) > std::chrono::milliseconds(100); }
+    bool outdated() const;
 
 private:
-    void pathfind_bfs(const Level& graph, glm::ivec2 origin, glm::ivec2 target)
-    {
-        /* Get neighbours and try all paths until we find target, or if we don't the Path will be empty */
-        std::queue<glm::ivec2> next_node = {};
-        next_node.push(origin);
+    /*!
+     * \brief pathfind_bfs uses breadth first search to find a suitable path
+     * \param graph is the graph that can be used to get neighbours
+     * \param origin is where we want to pathfind from
+     * \param target is the desired target position
+     */
+    void pathfind_bfs(const Level& graph, glm::ivec2 origin, glm::ivec2 target);
 
-        std::unordered_map<glm::ivec2, glm::ivec2, detail::custom_ivec2_hash> traceback = {};
-        traceback[origin] = origin;
+    /*!
+     * \brief pathfind_astar uses A* to find a suitable path
+     * \param graph is the graph that can be used to get neighbours
+     * \param origin is where we want to pathfind from
+     * \param target is the desired target position
+     */
+    void pathfind_astar(const Level& graph, glm::ivec2 origin, glm::ivec2 target);
 
-        while (!next_node.empty())
-        {
-            auto current = next_node.front();
-            next_node.pop();
-
-            if (current == target)
-            {
-                break;
-            }
-
-            for (auto next : graph.get_neighbours(current))
-            {
-                if (traceback.find(next) == traceback.end())
-                {
-                    next_node.push(next);
-                    traceback[next] = current;
-                }
-            }
-        }
-
-        /* Do traceback into stack */
-        for (auto it = target; it != origin; it = traceback.at(it))
-        {
-            m_directions.push(it - traceback.at(it));
-        }
-    }
+    /*!
+     * \brief heuristic compute manhattan distance between two points, used in astar as a heuristic
+     * \param from
+     * \param to
+     * \return manhattan distance
+     */
+    int heuristic(glm::ivec2 from, glm::ivec2 to);
 };
 }  // namespace pac
