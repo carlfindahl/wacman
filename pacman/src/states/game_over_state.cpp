4260a1ee7e08525301bab52b6048553f1f1e2d57
Carl Findahl <sportleroro@gmail.com>
2019-02-20 16:49:34 +0100

    Add Game Over state with a form to submit high score
    You can submit your name to the high score list.

--- pacman/src/states/game_over_state.cpp
+++ pacman/src/states/game_over_state.cpp
@@ -1,1 +1,55 @@
 #include "game_over_state.h"
+#include "main_menu_state.h"
+#include "state_manager.h"
+#include "config.h"
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include <cglutil.h>
+#include <imgui/imgui.h>
+
+namespace pac
+{
+void GameOverState::on_enter() { m_playername.fill('\0'); }
+
+void GameOverState::on_exit()
+{
+    /* Load and sort entries, then write back to file */
+    std::vector<ScoreEntry> entries = load_high_score_entries_from_file();
+    entries.push_back({m_playername.data(), m_score});
+    std::sort(entries.begin(), entries.end(), [](const ScoreEntry& a, const ScoreEntry& b) { return a.score > b.score; });
+    write_high_score_entries_to_file(entries);
+}
+
+bool GameOverState::update(float dt)
+{
+    ImGui::SetNextWindowPos({SCREEN_W / 2.f, SCREEN_H / 2.f + 60.f}, 0, {.5f, .5f});
+    ImGui::SetNextWindowSize({375.f, 80.f});
+    ImGui::Begin("New Hi-Score", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+
+    ImGui::Columns(3, nullptr, false);
+    ImGui::NextColumn();
+    ImGui::Text("Game Over!");
+    ImGui::NextColumn();
+    ImGui::NextColumn();
+    ImGui::Columns(1);
+
+    if (ImGui::Button("Submit"))
+    {
+        m_context.state_manager->clear();
+        m_context.state_manager->push<MainMenuState>(m_context);
+    }
+    ImGui::SameLine();
+    ImGui::InputText("Name", m_playername.data(), m_playername.size());
+
+    ImGui::Text("You scored %d points.", m_score);
+
+    ImGui::End();
+    return false;
+}
+
+bool GameOverState::draw() { return true; }
+
+}  // namespace pac
--- pacman/src/states/game_over_state.h
+++ pacman/src/states/game_over_state.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "states/state.h"
+
+#include <array>
+#include <cstdint>
+
+namespace pac
+{
+class GameOverState : public State
+{
+private:
+    std::array<char, 32> m_playername = {};
+
+    int32_t m_score = 0u;
+
+public:
+    using State::State;
+
+    GameOverState(GameContext context, int score) : State(context), m_score(score) {}
+
+    void on_enter() override;
+
+    void on_exit() override;
+
+    bool update(float dt) override;
+
+    bool draw() override;
+};
+}  // namespace pac
