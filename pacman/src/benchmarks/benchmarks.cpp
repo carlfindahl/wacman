0a0886ec3a6afc11a6053a01674b1314f30f1bf6
Carl Findahl <sportleroro@gmail.com>
2019-02-22 13:24:38 +0100

    Add some test benchmarks and parse arg for benching

--- pacman/src/benchmarks/benchmarks.cpp
+++ pacman/src/benchmarks/benchmarks.cpp
@@ -1,9 +1,53 @@
+#include "encrypt/vignere_encryptor.h"
+
 #include <benchmark/benchmark.h>
 
-//static void bench_encrypt(benchmark::State& state)
-//{
-//    for (auto _ : state)
-//    {
+/* TEST ENCRYPTION FUNCTION AND DECRYPTION FUNCTION ON SHORT / LONG STRINGS TO BE ABLE TO OPTIMIZE IT */
 
-//    }
-//}
+static void bench_encrypt_short(benchmark::State& state)
+{
+    pac::VignereEncryption enc("PACMAN");
+    std::string short_string("Hello you.");
+    for (auto _ : state)
+    {
+        enc.encrypt(short_string);
+    }
+}
+
+BENCHMARK(bench_encrypt_short);
+
+static void bench_encrypt_long(benchmark::State& state)
+{
+    pac::VignereEncryption enc("PACMAN");
+    std::string long_string("I am the most beaufitful person in the world and your name is pacman, but I do not care about it.");
+    for (auto _ : state)
+    {
+        enc.encrypt(long_string);
+    }
+}
+
+BENCHMARK(bench_encrypt_long);
+
+static void bench_decrypt_short(benchmark::State& state)
+{
+    pac::VignereEncryption enc("PACMAN");
+    std::string short_string("Hello you.");
+    for (auto _ : state)
+    {
+        enc.decrypt(short_string);
+    }
+}
+
+BENCHMARK(bench_decrypt_short);
+
+static void bench_decrypt_long(benchmark::State& state)
+{
+    pac::VignereEncryption enc("PACMAN");
+    std::string long_string("I am the most beaufitful person in the world and your name is pacman, but I do not care about it.");
+    for (auto _ : state)
+    {
+        enc.decrypt(long_string);
+    }
+}
+
+BENCHMARK(bench_decrypt_long);
--- pacman/src/main.cpp
+++ pacman/src/main.cpp
@@ -1,14 +1,34 @@
 #include "game.h"
-#include <config.h>
+#include "config.h"
+
+#include <string>
 
 #include <GLFW/glfw3.h>
+#include <benchmark/benchmark.h>
 
-int main()
+using namespace std::string_literals;
+
+int main(int argc, char* argv[])
 {
+#ifdef NDEBUG
+#ifdef PACMAN_USE_BENCHMARKS
+    if (argc == 2)
+    {
+        if (argv[1] == "bench"s)
+        {
+            benchmark::Initialize(&argc, argv);
+            benchmark::RunSpecifiedBenchmarks();
+            return 0;
+        }
+    }
+#endif
+#endif
+
+    /* Run game if we are not benchmarking */
     glfwInit();
 
     const auto title_string = std::string("OpenGL Pacman ") + pac::VERSION_STRING;
-    pac::Game game(title_string, {700u, 900u});
+    pac::Game game(title_string, {pac::SCREEN_W, pac::SCREEN_H});
     game.run();
 
     glfwTerminate();
