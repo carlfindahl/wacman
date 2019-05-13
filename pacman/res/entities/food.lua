7bdcf0bed3781307435910ad515be6c0d908ca14
Carl Findahl <sportleroro@gmail.com>
2019-05-13 20:45:35 +0200

    Add first LUA entity : Food

---
+++ pacman/res/entities/food.lua
@@ -0,0 +1,13 @@
+food = {
+    Position = { 
+        x = 5, 
+        y = 5
+    },
+    Pickup = { 
+        score = 50
+    },
+    Sprite = {
+        tint = { 1.0, 1.0, 1.0 },
+        index = 18 -- The Food Sprite IDX
+    }
+}
