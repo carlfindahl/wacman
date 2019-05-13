aed6ae5e8f9afc55b78eaf14a7c3a0fba59dfca4
Carl Findahl <sportleroro@gmail.com>
2019-05-13 12:29:50 +0200

    Create an entity factory that can read from .lua files

---
+++ pacman/src/entity/factory.cpp
@@ -0,0 +1,90 @@
+#include "factory.h"
+
+namespace pac
+{
+uint32_t EntityFactory::spawn(sol::state_view& state, const std::string& name)
+{
+    auto e = m_registry.create();
+
+    /* Map entity names to filepaths */
+    if (m_entity_path_map.find(name) == m_entity_path_map.end())
+    {
+        auto path = find_entity_path(name);
+        if (path)
+        {
+            m_entity_path_map[name] = *path;
+        }
+        else
+        {
+            return entt::null;
+        }
+    }
+
+    /* Get the filepath assosciated with the entity name */
+    const auto& filepath = m_entity_path_map.at(name);
+    GFX_DEBUG("Making entity from file: %s", filepath.filename().c_str());
+
+    /* Load script, and iterate all keys in table */
+    state.script_file(filepath.string());
+    sol::table table = state[filepath.filename().stem().string()];
+    for (const auto& k : table)
+    {
+        /* Hash key for faster compares, and then get the component */
+        sol::table component = table[k.first.as<const char*>()];
+
+        /* Check all known components, and attach it to the entity */
+        if (auto pos = m_component_map.find(k.first.as<std::string>()); pos != m_component_map.end())
+        {
+            m_component_map.at(k.first.as<std::string>())(state, component, e);
+        }
+    }
+}
+
+std::optional<std::filesystem::path> EntityFactory::find_entity_path(const std::string& name)
+{
+    std::stack<std::filesystem::path> to_visit = {};
+    to_visit.push(cgl::native_absolute_path("res/entities"));
+
+    /* Until there are no more subdirectories, or we have found the entity */
+    while (!to_visit.empty())
+    {
+        auto current = to_visit.top();
+        to_visit.pop();
+        for (auto entry : std::filesystem::directory_iterator(current))
+        {
+            /* If it is a directory, store it for future visits */
+            if (entry.is_directory())
+            {
+                to_visit.push(entry.path());
+            }
+            else if (entry.is_regular_file())
+            {
+                if (entry.path().filename().string() == name + ".lua")
+                {
+                    return entry.path();
+                }
+            }
+        }
+    }
+
+    GFX_WARN("Could not find entity'%s', ensure spelling is correct, or that entity exists!", name.c_str());
+    return std::nullopt;
+}
+
+void EntityFactory::make_sprite_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_animsprite_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_position_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_movement_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_player_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_input_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_pickup_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+void EntityFactory::make_collision_component(sol::state_view& state, sol::table& comp, uint32_t e) {}
+
+}  // namespace pac
---
+++ pacman/src/entity/factory.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <stack>
+#include <string>
+#include <cstdint>
+#include <optional>
+#include <filesystem>
+
+#include <gfx.h>
+#include <cglutil.h>
+#include <robinhood/robinhood.h>
+#include <sol/state_view.hpp>
+#include <entt/entity/registry.hpp>
+
+namespace pac
+{
+class EntityFactory
+{
+private:
+    /* Factory registry */
+    entt::registry& m_registry;
+
+    /* Cached entity paths */
+    robin_hood::unordered_map<std::string, std::filesystem::path> m_entity_path_map{};
+
+    /* Map that maps hashed component names to component creation functions */
+    robin_hood::unordered_map<std::string, void (*)(sol::state_view&, const sol::table&, uint32_t)> m_component_map{
+        {"Sprite", std::mem_fn(&EntityFactory::make_sprite_component)}};
+
+public:
+    EntityFactory(entt::registry& registry) : m_registry(registry) {}
+
+    /*!
+     * \brief spawn spawns a new entity from a resource file
+     * \param state is the lua state where the entity is defined
+     * \param name is the name of the entity
+     * \return the newly spawned entity
+     */
+    uint32_t spawn(sol::state_view& state, const std::string& name);
+
+private:
+    /*!
+     * \brief find_entity_path finds the path of the .lua file where the entity data is stored
+     * \param name is the name of the entity to look for
+     * \return the path if any
+     */
+    std::optional<std::filesystem::path> find_entity_path(const std::string& name);
+
+    /* Factory functions for each component type */
+    void make_sprite_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_animsprite_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_position_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_movement_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_player_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_input_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_pickup_component(sol::state_view& state, sol::table& comp, uint32_t e);
+    void make_collision_component(sol::state_view& state, sol::table& comp, uint32_t e);
+};
+
+}  // namespace pac
