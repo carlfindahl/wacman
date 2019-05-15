675585f894819454f78f60c25bd2aeaf31ac571d
Carl Findahl <sportleroro@gmail.com>
2019-05-15 10:57:25 +0200

    Add components to ghost.lua

---
+++ pacman/res/entities/ghost.lua
@@ -0,0 +1,49 @@
+ghost = {
+    Collision = {},
+    Position = {
+        x = 0,
+        y = 0
+    },
+    Movement = {
+        speed = 3.5        
+    },
+    AnimationSprite = {
+        fps = 24.0,
+        tint = { 1.0, 1.0 , 1.0 },
+        starting = "up",
+        sprites = {
+            up = {
+                file = "pacman.png",
+                startX = 4,
+                startY = 1,
+                size = 70,
+                cols = 2,
+                length = 2
+            },
+            down = {
+                file = "pacman.png",
+                startX = 4,
+                startY = 0,
+                size = 70,
+                cols = 2,
+                length = 2
+            },
+            left = {
+                file = "pacman.png",
+                startX = 4,
+                startY = 2,
+                size = 70,
+                cols = 2,
+                length = 2
+            },
+            right = {
+                file = "pacman.png",
+                startX = 4,
+                startY = 3,
+                size = 70,
+                cols = 2,
+                length = 2
+            }
+        }
+    }
+}
