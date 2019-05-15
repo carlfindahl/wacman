5607192dd33e5f7aa264a5a5203f52d34506c077
Carl Findahl <sportleroro@gmail.com>
2019-05-15 10:59:36 +0200

    Add pickup entities

---
+++ pacman/res/entities/banana.lua
@@ -0,0 +1,13 @@
+banana = {
+    Position = { 
+        x = 0, 
+        y = 0
+    },
+    Pickup = { 
+        score = 250
+    },
+    Sprite = {
+        tint = { 1.0, 1.0, 1.0 },
+        index = 16 -- The Food Sprite IDX
+    }
+}
---
+++ pacman/res/entities/ghostkiller.lua
@@ -0,0 +1,13 @@
+ghostkiller = {
+    Position = { 
+        x = 0, 
+        y = 0
+    },
+    Pickup = { 
+        score = 150
+    },
+    Sprite = {
+        tint = { 1.0, 1.0, 1.0 },
+        index = 19 -- The Food Sprite IDX
+    }
+}
